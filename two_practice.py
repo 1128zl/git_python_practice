d185168592df654c32ef1c6ff935551fb41b430f
1128zl <2265335998@qq.com>
2023-10-07 17:23:10 +0800

    first

---
+++ two_practice.py
@@ -0,0 +1,83 @@
+# 变量
+'''
+message = "Hello Python world!"
+print (message)
+
+message = "Hello Python Crash Course world!"
+print (message)
+'''
+
+# 字符串
+# 单引号或者双引号引起的都是字符串
+
+'''
+print("hello git.")
+print('hello git.')
+'''
+
+
+
+
+# 2.3
+'''
+name="eric"
+last_name="would you like to learn some Python today"
+full_time=f"{name.title()} {last_name}"
+# 将名字处理成首字母大写，其余字母小写，并和last_time进行连接
+print(f"Hello, {full_time}?")
+'''
+
+# 2.4
+
+'''
+name="jack"
+print(name.upper())
+print(name.lower())
+print(name.title())
+'''
+
+# 2.5  2.6
+
+'''
+name ="albert einstein"
+word ='"a person who never made a mistake never tried angthing new."'
+print(f"{name.title()} once said, {word.title()}")
+'''
+
+# 2.7
+'''
+name ="  helen  "
+print(name.strip())
+print("his name is :\n\t"+ name.strip())
+'''
+
+# 2.8
+'''
+filename ="python_notes.txt"
+print(filename)
+filename_1=filename.removesuffix("txt")
+print(filename_1)
+'''
+
+# 2.9
+'''
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+'''
+# 2.10
+'''
+number =19
+string ="My favorite number is " +str(number)
+print(string)
+'''
+# 2.11
+'''
+# name:zl date:2023.10.7
+# python语言打印hello git 语句
+print("hello git")
+'''
+# 2.12
+
+import  this
